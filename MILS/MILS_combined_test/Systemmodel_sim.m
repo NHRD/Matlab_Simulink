c3c650d844d1e7f87c58a4d1131458b235894207
Naohisa Harada <naohisa.harada@gmail.com>
2020-08-05 15:50:45 +0900

    a

---
+++ MILS/MILS_combined_test/Systemmodel_sim.m
@@ -0,0 +1,47 @@
+clear
+close all
+clc
+
+%rot sensor parameters
+alpha_T = 1.5/1000;
+
+%motor driver parameters
+alpha_i = 5/37.5;
+
+%system input voltage
+Vin = 24;
+
+%motor parameters
+R = 5.7;
+L = 0.2;
+Ke = 7.16e-2;
+Kt = 7.2e-2;
+Jm = 0.11e-3;
+
+%disk parameters
+Ji = 1.3e-3;
+D = 6.0e-5;
+
+%A/D converter parameters
+
+V_A_max = 5;
+V_A_min = 0;
+n = 10;
+alpha_A = (2^n - 1)/(V_A_max - V_A_min);
+
+%pulse generator parameters
+
+m = 8;
+alpha_P = 100/(2^m - 1);
+
+%algorithm model parameters
+
+u_min = 0;
+u_max = 100;
+
+%sim settings
+smp = 0.001;
+Endtime = 60;
+filename = "Systemmodel.slx";
+open(filename)
+sim(filename)
---
+++ MILS/controller_model/controllermodel/controller_model_sim.m
@@ -0,0 +1,48 @@
+clear
+close all
+clc
+
+%rot sensor parameters
+
+alpha_T = 1.5/1000;
+omega_d = 2*pi;
+
+%motor drv and current sensor parameters
+
+alpha_i = 5/37.5;
+Vin = 24;
+u_M_d = 10;
+i_M_d = 3;
+
+%A/D converter parameters
+
+V_A_max = 5;
+V_A_min = 0;
+n = 10;
+alpha_A = (2^n - 1)/(V_A_max - V_A_min);
+%v_A_d = 1;
+
+%pulse generator parameters
+
+m = 8;
+alpha_P = 100/(2^m - 1);
+%d_P_d = 5;
+
+%encoder and decoder parameters
+
+u_min = 0;
+u_max = 100;
+%d_N_d = (2^n-1)/60;
+%d_i_d = (2^n-1)/60;
+
+%input parameters
+v_i_d = 1/12;
+v_N_d = 1/12;
+
+%Simulink contorl
+
+smp = 0.0001;
+Endtime = 60;
+filename = "controller_model.slx";
+open(filename)
+sim(filename)
